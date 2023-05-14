dbe4df5c326f4d9645c7e13640e1f0960b49326d
dev-rafa1707 <rafa1707@gmail.com>
2023-05-14 09:38:12 -0300

    add ex 43

--- Guanabara-ex43.py
+++ Guanabara-ex43.py
@@ -5,3 +5,24 @@
 # - 25 até 30: Sobrepeso
 # - 30 até 40: Obesidade
 # - Acima de 40: Obesidade Mórbida
+
+peso = float(input('Informe o peso: '))
+altura = float(input('Informe a altura: '))
+
+def calculaIMC(peso, altura):
+    imc = peso / (altura * altura)
+    
+    if imc < 18.5:
+        return f'Abaixo do peso pois o IMC é {imc}'
+    elif imc > 18.5 and imc < 25:
+        return f'Peso ideal pois o IMC é {imc}'
+    elif imc > 25 and imc < 30:
+        return f'Sobrepeso pois o IMC é {imc}'
+    elif imc > 30 and imc < 40:
+        return f'Obesidade pois o IMC é {imc}'
+    elif imc > 40:
+        return f'Obesidade mórbida pois o IMC é {imc}'
+    else:
+        return f'Verifique os dados'
+
+imc = print(calculaIMC(peso, altura))
