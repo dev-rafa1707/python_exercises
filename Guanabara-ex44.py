dda870e9b3bbecc66c5d61ad0f87ebeeff320457
dev-rafa1707 <rafa1707@gmail.com>
2023-05-14 09:48:33 -0300

    add ex 44

--- Guanabara-ex44.py
+++ Guanabara-ex44.py
@@ -4,3 +4,13 @@
 # - à vista no cartão: 5% de desconto
 # - em até 2x no cartão: preço formal
 # - 3x ou mais no cartão: 20% de juros
+
+preco = float(input('Informe o valor do produto:'))
+
+# print(- à vista dinheiro/cheque: 10% de desconto
+# - à vista no cartão: 5% de desconto
+# - em até 2x no cartão: preço formal
+# - 3x ou mais no cartão: 20% de juros
+
+
+# condicao = int(input)
