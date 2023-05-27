1c6de3a22bbffc7cf5e4589580f1cdc351cb3bef
dev-rafa1707 <rafa1707@gmail.com>
2023-05-27 15:19:24 -0300

    add ex 44

--- Guanabara-ex44.py
+++ Guanabara-ex44.py
@@ -7,10 +7,28 @@
 
 preco = float(input('Informe o valor do produto:'))
 
-# print(- à vista dinheiro/cheque: 10% de desconto
-# - à vista no cartão: 5% de desconto
-# - em até 2x no cartão: preço formal
-# - 3x ou mais no cartão: 20% de juros
+print('Selecione 1 para pagamento à vista dinheiro/cheque: 10 de desconto')
+print('Selecione 2 para pagamento à vista no cartão: 5 de desconto')
+print('Selecione 3 para pagamento em até 2x no cartão: preço formal')
+print('Selecione 4 para pagamento 3x ou mais no cartão: 20 de juros')
 
 
-# condicao = int(input)
+condicao = int(input('Informe a condição:'))
+
+def calculaCondicao(preco, condicao):
+    if condicao == 1:
+        preco = 0.9 * preco
+        return preco
+    elif condicao == 2:
+        preco = 0.95 * preco
+        return preco
+    elif condicao == 3:
+        return preco
+    elif condicao == 4:
+        preco = 1.2 * preco
+        return preco
+    else:
+        print('Opção inválida')
+
+cliente = calculaCondicao(preco,condicao)
+print(cliente)
