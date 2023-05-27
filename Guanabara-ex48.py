10804e2104eb5fa8eca0b9725d4529a38cd02cd2
dev-rafa1707 <rafa1707@gmail.com>
2023-05-27 15:30:13 -0300

    add ex 48

--- Guanabara-ex48.py
+++ Guanabara-ex48.py
@@ -1,1 +1,2 @@
-# Exercício Python 048
+# Exercício Python 048: Faça um programa que calcule a soma entre todos os números 
+# que são múltiplos de três e que se encontram no intervalo de 1 até 500.
