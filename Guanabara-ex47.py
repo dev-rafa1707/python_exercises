fc37dcde60ff23ded5fef09a27964522d3e717fc
dev-rafa1707 <rafa1707@gmail.com>
2023-05-28 18:09:43 -0300

    add resolucao ex 47

--- Guanabara-ex47.py
+++ Guanabara-ex47.py
@@ -1,2 +1,8 @@
 # Exercício Python 047: Crie um programa que mostre na tela 
 # todos os números pares que estão no intervalo entre 1 e 50.
+
+contador = 2
+
+while contador <= 50:
+    print(contador)
+    contador +=2
