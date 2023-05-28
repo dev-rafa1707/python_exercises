6a36ca6acdc2d56d17dfb5f32cb1a059f0af1ab9
dev-rafa1707 <rafa1707@gmail.com>
2023-05-28 18:49:25 -0300

    add ex 45

--- Guanabara-ex45.py
+++ Guanabara-ex45.py
@@ -1,1 +1,41 @@
 # Exercício Python 045: Crie um programa que faça o computador jogar Jokenpô com você.
+
+import random
+from random import randint
+
+
+print('Suas opções')
+print('[1] para pedra')
+print('[2] para papel')
+print('[3] para tesoura')
+
+jogador = int(input('Informe sua jogada: '))
+
+
+def janKenPo(jogador):
+    computador = random.randint(1,3)
+
+    if computador == 1:
+        print('Computador escolheu pedra')
+    elif computador == 2:
+        print('Computador escolheu papel')
+    elif computador == 3:
+        print('Computador escolheu tesoura')   
+
+
+    if jogador == computador:
+        return('Empate')
+    elif jogador == 1 and computador == 2:
+        return('Você perdeu')
+    elif jogador == 1 and computador == 3:
+        return('Você ganhou')
+    elif jogador == 2 and computador == 1:
+        return('Você ganhou')
+    elif jogador == 2 and computador == 3:
+        return('Você perdeu')
+    elif jogador == 3 and computador == 1:
+        return('Você perdeu')
+    elif jogador == 3 and computador == 2:
+        return('Você ganhou')
+    
+jogada = print(janKenPo(jogador))
