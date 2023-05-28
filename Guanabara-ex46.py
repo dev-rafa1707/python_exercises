861b4453a9ba931495ef1690c0b85de6f47cad93
dev-rafa1707 <rafa1707@gmail.com>
2023-05-28 18:17:02 -0300

    add ex 46

--- Guanabara-ex46.py
+++ Guanabara-ex46.py
@@ -1,3 +1,14 @@
 # Exercício Python 046: Faça um programa que mostre na tela uma contagem regressiva
 #  para o estouro de fogos de artifício, indo de 10 até 0,
 # com uma pausa de 1 segundo entre eles.
+
+import time
+
+def contagemRegressiva(contador):
+    while contador > 0:
+        print(contador)
+        time.sleep(1)
+        contador -= 1
+
+contagem = contagemRegressiva(10)
+
