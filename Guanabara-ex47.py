54c669095b7e7d56a2eed1ba87ff9f1f7c1b26e0
dev-rafa1707 <rafa1707@gmail.com>
2023-05-28 18:18:56 -0300

    add funcao ex 47

--- Guanabara-ex47.py
+++ Guanabara-ex47.py
@@ -1,8 +1,11 @@
 # Exercício Python 047: Crie um programa que mostre na tela 
 # todos os números pares que estão no intervalo entre 1 e 50.
 
-contador = 2
 
-while contador <= 50:
-    print(contador)
-    contador +=2
+def imprimeNumeros():
+    contador = 2
+    while contador <= 50:
+        print(contador)
+        contador +=2
+
+numero = imprimeNumeros()
