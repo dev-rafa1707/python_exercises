ba29b8c9690a16b9cd4e23fdc47dea05391b395c
dev-rafa1707 <rafa1707@gmail.com>
2023-06-11 11:00:25 -0300

    add resolucao ex48

--- Guanabara-ex48.py
+++ Guanabara-ex48.py
@@ -1,2 +1,19 @@
 # Exercício Python 048: Faça um programa que calcule a soma entre todos os números 
 # que são múltiplos de três e que se encontram no intervalo de 1 até 500.
+
+
+def calculaNumero():
+    contador = 0
+    somatorio = 0
+    for i in range(1,501,2):
+        if i % 3 == 0:
+            contador +=1
+            somatorio += i
+    return(contador, somatorio)
+
+
+
+
+contador, somatorio = calculaNumero()
+
+print(f'A soma de todos os {contador} números é igual a {somatorio}')
