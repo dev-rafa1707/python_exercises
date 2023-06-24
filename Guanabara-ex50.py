af7d4b4aa221b66f8bf35a3ec8652a1962064364
dev-rafa1707 <rafa1707@gmail.com>
2023-06-24 14:54:21 -0300

    feat: add input insere numeros

--- Guanabara-ex50.py
+++ Guanabara-ex50.py
@@ -1,3 +1,11 @@
 # Exercício Python 050: Desenvolva um programa que leia seis números inteiros 
 # e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
 
+# def somarPares()
+numeros = []
+
+for i in range(6):
+    numero = input('Informe um numero inteiro: ')
+    numero = numeros.append(numero)
+print(numeros)
+
