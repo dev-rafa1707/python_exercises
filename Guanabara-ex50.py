a47ce86c00c60669bdaf58a787277da04411e5e7
dev-rafa1707 <rafa1707@gmail.com>
2023-06-24 15:01:32 -0300

    feat: add def somarPares

--- Guanabara-ex50.py
+++ Guanabara-ex50.py
@@ -1,12 +1,15 @@
 # Exercício Python 050: Desenvolva um programa que leia seis números inteiros 
 # e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
 
-# def somarPares()
-numeros = []
+def somarPares():
+    numeros = []
+    soma = 0
+    for i in range(6):
+        numero = int(input('Informe um numero inteiro: '))
+        if numero % 2 == 0:
+        # numero = numeros.append(numero)
+            soma = soma + numero
+# print(numeros)
+    return soma
 
-for i in range(6):
-    numero = int(input('Informe um numero inteiro: '))
-    if numero % 2 == 0:
-        numero = numeros.append(numero)
-print(numeros)
-
+soma = print(somarPares())
