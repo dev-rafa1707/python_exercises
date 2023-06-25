3e03934916763df77be604f6412a84cf10fcb45c
dev-rafa1707 <rafa1707@gmail.com>
2023-06-25 18:39:51 -0300

    feat add primeiro termo e razao da PA

--- Guanabara-ex51.py
+++ Guanabara-ex51.py
@@ -1,3 +1,8 @@
 # Exercício Python 051: Desenvolva um programa que leia o primeiro termo 
 # e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.
 
+primeiroTermo = int(input("Informe o primeiro termo da PA: "))
+razao = int(input("Informe a razão da PA: "))
+
+print(primeiroTermo)
+print(razao)
