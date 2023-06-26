a78b04d287b42f53d27dc3c05f104cddb83d258d
dev-rafa1707 <rafa1707@gmail.com>
2023-06-26 15:19:54 -0300

    add ex 52

--- Guanabara-ex52.py
+++ Guanabara-ex52.py
@@ -1,3 +1,13 @@
 # Exercício Python 052: Faça um programa que leia um número inteiro 
 # e diga se ele é ou não um número primo.
 
+num = int(input("Informe um número inteiro: "))
+count = 0
+
+for i in range(1,num+1):
+    if num % i == 0:
+        count += 1
+if count == 2:
+    print("Número primo")
+else:
+    print("Número não é primo")
