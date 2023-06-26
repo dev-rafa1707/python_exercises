c27c39d79d841e771d4cc0cebb9b151b10121774
dev-rafa1707 <rafa1707@gmail.com>
2023-06-26 15:23:07 -0300

    fead: add def numeroPrimo

--- Guanabara-ex52.py
+++ Guanabara-ex52.py
@@ -1,13 +1,18 @@
 # Exercício Python 052: Faça um programa que leia um número inteiro 
 # e diga se ele é ou não um número primo.
 
-num = int(input("Informe um número inteiro: "))
-count = 0
 
-for i in range(1,num+1):
-    if num % i == 0:
-        count += 1
-if count == 2:
-    print("Número primo")
-else:
-    print("Número não é primo")
+def numeroPrimo(num):
+    num = int(input("Informe um número inteiro: "))
+    count = 0
+
+    for i in range(1,num+1):
+        if num % i == 0:
+            count += 1
+    if count == 2:
+        print("Número primo")
+    else:
+        print("Número não é primo")
+
+
+teste = numeroPrimo(17)
